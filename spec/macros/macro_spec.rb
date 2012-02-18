f3ae39e57411f7ca501e3c0d051299af24c42d59
Omar Awile <omar.awile@inf.ethz.ch>
2012-02-18 18:51:02 +0100

    added specs for Macro

--- spec/macros/macro_spec.rb
+++ spec/macros/macro_spec.rb
@@ -19,5 +19,24 @@ module CG
         REPLACEMENT
       end
     end
+    
+    context "macro loading" do
+      it "should create Macro instances for all macro defintions in a file" do
+        path = "examples/testdata/macros/function_call.mac"
+        macros = Macro.load(path)
+        macros.keys.size.should == 3
+        macros.keys.should include "simple"
+        macros.keys.should include "with_ruby"
+        macros.keys.should include "override"
+      end
+      it "should create Macro instances for all macro defintions in a string" do
+        path = "examples/testdata/macros/function_call.mac"
+        macros = Macro.load_file File.open(path)
+        macros.keys.size.should == 3
+        macros.keys.should include "simple"
+        macros.keys.should include "with_ruby"
+        macros.keys.should include "override"
+      end
+    end
   end
 end
