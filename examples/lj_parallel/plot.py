218b1005879e549ec3d6bfedb366ad2501b95501
Omar Awile <omar.awile@gmail.com>
2012-10-10 22:08:47 +0200

    Another version of the lj client

---
+++ examples/lj_parallel/plot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+
+def plot_tot(ival):
+    etot = []
+    epot = []
+    ekin = []
+    dist = []
+    f = open('energy.dat')
+    f.readline()
+    i = 0
+    for l in f:
+        r = l.strip().split()
+        if (i % ival == 0):
+            etot.append(float(r[1]))
+            ekin.append(float(r[2]))
+            epot.append(float(r[3]))
+            dist.append(float(r[4]))
+        i = i + 1
+    f.close()
+    t = [i*ival for i in range(len(epot))]
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+
+    ax1.plot(t,etot,'b',label='$E_{tot}$')
+    ax1.plot(t,epot,'r',label='$E_{pot}$')
+    
+    ax2 = ax1.twinx()
+    ax2.plot(t,dist,'g',label='$\lambda$')
+    
+    
+    ax1.legend(loc=3)
+    ax2.legend(loc=0)
+    ax1.set_xlabel('timestep')
+    ax1.set_ylabel('$E(r)$')
+    ax2.set_ylabel('displacement since nlist')
+    ax1.set_ylim([-6.0e-9,-3.0e-9])
+    plt.show()
+
+plot_tot(10)
