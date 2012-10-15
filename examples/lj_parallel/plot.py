f7a8e65b89f8bc607a9a07fb4337694619cbebc0
Omar Awile <omar.awile@gmail.com>
2012-10-15 16:09:55 +0200

    Working parallel LJ

--- examples/lj_parallel/plot.py
+++ examples/lj_parallel/plot.py
@@ -5,8 +5,9 @@ def plot_tot(ival):
     etot = []
     epot = []
     ekin = []
-    dist = []
+    #dist = []
     f = open('energy.dat')
+    f.readline()
     f.readline()
     i = 0
     for l in f:
@@ -15,7 +16,7 @@ def plot_tot(ival):
             etot.append(float(r[1]))
             ekin.append(float(r[2]))
             epot.append(float(r[3]))
-            dist.append(float(r[4]))
+            #dist.append(float(r[4]))
         i = i + 1
     f.close()
     t = [i*ival for i in range(len(epot))]
@@ -26,16 +27,16 @@ def plot_tot(ival):
     ax1.plot(t,etot,'b',label='$E_{tot}$')
     ax1.plot(t,epot,'r',label='$E_{pot}$')
     
-    ax2 = ax1.twinx()
-    ax2.plot(t,dist,'g',label='$\lambda$')
+    #ax2 = ax1.twinx()
+    #ax2.plot(t,dist,'g',label='$\lambda$')
     
     
     ax1.legend(loc=3)
-    ax2.legend(loc=0)
+    #ax2.legend(loc=0)
     ax1.set_xlabel('timestep')
     ax1.set_ylabel('$E(r)$')
-    ax2.set_ylabel('displacement since nlist')
-    ax1.set_ylim([-6.0e-9,-3.0e-9])
+    #ax2.set_ylabel('displacement since nlist')
+    #ax1.set_ylim([-1.0e-5,-1.0e-6])
     plt.show()
 
 plot_tot(10)
